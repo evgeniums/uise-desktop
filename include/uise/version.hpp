cc467f6054e39a1669c6bf5ce2dcae30519873f6
Evgeny Sidorov <evgeniums@dracosha.com>
2021-01-13 15:49:22 +0300

    Add version.hpp

---
+++ include/uise/version.hpp
@@ -0,0 +1,54 @@
+/**
+@copyright Evgeny Sidorov 2021
+
+This software is dual-licensed. Choose the appropriate license for your project.
+
+1. For open source projects this software is distrubuted under the GNU GENERAL PUBLIC LICENSE, Version 3.0.
+(See accompanying file LICENSE-GPLv3.md or copy at https://www.gnu.org/licenses/gpl-3.0.txt)
+
+2. For proprietary license contact evgeniums@dracosha.com.
+
+*/
+
+/****************************************************************************/
+
+/** \file uise/version.hpp
+*
+*  Defines macros for tracking the version of the library.
+*
+*/
+
+/****************************************************************************/
+
+#ifndef UISE_VERSION_HPP
+#define UISE_VERSION_HPP
+
+//! @internal
+//! Transforms a (version, revision, patchlevel) triple into a number of the
+//! form 0xVVRRPPPP to allow comparing versions in a normalized way.
+//!
+//! See http://sourceforge.net/p/predef/wiki/VersionNormalization.
+#define UISE_CONFIG_VERSION(version, revision, patch) \
+    (((version) << 24) + ((revision) << 16) + (patch))
+
+//! @ingroup group-config
+//! Macro expanding to the major version of the library, i.e. the `x` in `x.y.z`.
+#define UISE_MAJOR_VERSION 0
+
+//! @ingroup group-config
+//! Macro expanding to the minor version of the library, i.e. the `y` in `x.y.z`.
+#define UISE_MINOR_VERSION 0
+
+//! @ingroup group-config
+//! Macro expanding to the patch level of the library, i.e. the `z` in `x.y.z`.
+#define UISE_PATCH_VERSION 1
+
+//! @ingroup group-config
+//! Macro expanding to the full version of the library, in hexadecimal
+//! representation.
+#define UISE_VERSION                                            \
+    UISE_CONFIG_VERSION(UISE_MAJOR_VERSION,                     \
+                        UISE_MINOR_VERSION,                     \
+                        UISE_PATCH_VERSION)                     \
+
+#endif // UISE_VERSION_HPP
