cb8a8b063b206a7b69f3d517d09a94c57a4e3958
Evgeny Sidorov <evgeniums@dracosha.com>
2021-01-31 18:23:42 +0300

    Add comments to Layout class.

--- include/uise/desktop/utils/layout.hpp
+++ include/uise/desktop/utils/layout.hpp
@@ -31,16 +31,29 @@ This software is dual-licensed. Choose the appropriate license for your project.
 
 UISE_DESKTOP_NAMESPACE_BEGIN
 
+/**
+ * @brief Hepler class to work with Qt layouts.
+ */
 class Layout
 {
     public:
 
+        /**
+         * @brief Clear layout.
+         * @param layout Layout to clear.
+         */
         static void clear(QLayout* layout)
         {
             layout->setContentsMargins(0,0,0,0);
             layout->setSpacing(0);
         }
 
+        /**
+         * @brief Create layout for widget.
+         * @param widget Widget to create layout for.
+         * @param reset If set (default) then reset created layout.
+         * @return Created layout.
+         */
         template <typename LayoutT, typename WidgetT>
         static LayoutT* create(WidgetT* widget, bool reset=true)
         {
@@ -54,24 +67,49 @@ class Layout
             return layout;
         }
 
+        /**
+         * @brief Create QVBoxLayout for widget.
+         * @param widget Widget to create layout for.
+         * @param reset If set (default) then reset created layout.
+         * @return Created layout.
+         */
         template <typename WidgetT>
         static QVBoxLayout* vertical(WidgetT* widget, bool reset=true)
         {
             return create<QVBoxLayout>(widget,reset);
         }
 
+        /**
+         * @brief Create QHBoxLayout for widget.
+         * @param widget Widget to create layout for.
+         * @param reset If set (default) then reset created layout.
+         * @return Created layout.
+         */
         template <typename WidgetT>
         static QHBoxLayout* horizontal(WidgetT* widget, bool reset=true)
         {
             return create<QHBoxLayout>(widget,reset);
         }
 
+        /**
+         * @brief Create QGridLayout for widget.
+         * @param widget Widget to create layout for.
+         * @param reset If set (default) then reset created layout.
+         * @return Created layout.
+         */
         template <typename WidgetT>
         static QGridLayout* grid(WidgetT* widget, bool reset=true)
         {
             return create<QGridLayout>(widget,reset);
         }
 
+        /**
+         * @brief Create QBoxLayout for widget.
+         * @param widget Widget to create layout for.
+         * @param orientation Orintation of the layout.
+         * @param reset If set (default) then reset created layout.
+         * @return Created layout.
+         */
         template <typename WidgetT>
         static QBoxLayout* box(WidgetT* widget, Qt::Orientation orientation, bool reset=true)
         {
