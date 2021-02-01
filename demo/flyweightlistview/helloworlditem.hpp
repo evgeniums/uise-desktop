8e81e4a80fc06225165d848e163f0f9786ee53d0
Evgeny Sidorov <evgeniums@dracosha.com>
2021-02-01 14:11:46 +0300

    Fix typo

--- demo/flyweightlistview/helloworlditem.hpp
+++ demo/flyweightlistview/helloworlditem.hpp
@@ -12,9 +12,9 @@ This software is dual-licensed. Choose the appropriate license for your project.
 
 /****************************************************************************/
 
-/** @file demo/flyweightlistview/main.cpp
+/** @file demo/flyweightlistview/helloworlditem.cpp
 *
-*  Demo application of FlyweightListView.
+*  List item class for FlyweightListView demo and test applications.
 *
 */
 
