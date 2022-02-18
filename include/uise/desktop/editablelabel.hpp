134340bb185a0b1a9bd9a7f6736a55d40f3423ff
Evgeny Sidorov <evgeniums@dracosha.com>
2022-02-18 18:11:14 +0300

    Done EditableLabel

--- include/uise/desktop/editablelabel.hpp
+++ include/uise/desktop/editablelabel.hpp
@@ -33,9 +33,14 @@ This software is dual-licensed. Choose the appropriate license for your project.
 #include <QTimeEdit>
 #include <QDateTimeEdit>
 
+#include <QBoxLayout>
+#include <QEvent>
+
 #include <uise/desktop/uisedesktop.hpp>
 
 UISE_DESKTOP_NAMESPACE_BEGIN
+
+class EditableLabelFormatter;
 
 class UISE_DESKTOP_EXPORT EditableLabel : public QFrame
 {
@@ -74,6 +79,15 @@ class UISE_DESKTOP_EXPORT EditableLabel : public QFrame
 
         virtual QWidget* editableWidget() const noexcept =0;
 
+        void setFormatter(EditableLabelFormatter* formatter) noexcept
+        {
+            m_formatter=formatter;
+        }
+        EditableLabelFormatter* formatter() const noexcept
+        {
+            return m_formatter;
+        }
+
     public slots:
 
         void setEditable(bool enable)
@@ -82,25 +96,63 @@ class UISE_DESKTOP_EXPORT EditableLabel : public QFrame
             editableWidget()->setVisible(enable);
         }
 
+        virtual void apply() = 0;
+
+        void cancel()
+        {
+            setEditable(false);
+            restoreWidgetValue();
+        }
+
     signals:
 
         void valueSet();
 
     protected:
 
-        void setText(const QString& text)
+        QBoxLayout* boxLayout() const noexcept
         {
-            m_label->setText(text);
+            return m_layout;
         }
+
+        virtual void restoreWidgetValue() =0;
+
+        bool eventFilter(QObject *watched, QEvent *event) override;
 
     private:
 
         Type m_type;
         QLabel* m_label;
+        QBoxLayout* m_layout;
+        EditableLabelFormatter* m_formatter;
 };
 
-class EditableLabelFormat
+class UISE_DESKTOP_EXPORT EditableLabelFormatter
 {
+    public:
+
+        ~EditableLabelFormatter() = default;
+
+        EditableLabelFormatter()=default;
+        EditableLabelFormatter(const EditableLabelFormatter&)=default;
+        EditableLabelFormatter(EditableLabelFormatter&&)=default;
+        EditableLabelFormatter& operator = (const EditableLabelFormatter&)=default;
+        EditableLabelFormatter& operator = (EditableLabelFormatter&&)=default;
+
+        virtual QString format(EditableLabel::Type type, const QVariant& value) =0;
+
+        template <EditableLabel::Type Type, typename ValueT>
+        static void loadLabel(QLabel* label, EditableLabelFormatter* formatter, const std::function<ValueT ()>& valueGetter, const std::function<QString (const ValueT&)>& defaultFormatter)
+        {
+            if (formatter==nullptr)
+            {
+                label->setText(defaultFormatter(valueGetter()));
+            }
+            else
+            {
+                label->setText(formatter->format(Type,valueGetter()));
+            }
+        }
 };
 
 template <EditableLabel::Type Type>
@@ -113,10 +165,9 @@ struct EditableLabelTraits<EditableLabel::Type::Text>
 {
     using type=QLineEdit;
 
-    static void loadLabel(QLabel* label, type* widget, EditableLabelFormat* format=nullptr)
+    static void loadLabel(QLabel* label, type* widget, EditableLabelFormatter* formatter=nullptr)
     {
-        std::ignore=format;
-        label->setText(widget->text());
+        EditableLabelFormatter::loadLabel<EditableLabel::Type::Text,QString>(label,formatter,[widget](){return widget->text();},[](const QString& val){return val;});
     }
 
     static auto value(const type* widget)
@@ -135,10 +186,11 @@ struct EditableLabelTraits<EditableLabel::Type::Int>
 {
     using type=QSpinBox;
 
-    static void loadLabel(QLabel* label, type* widget, EditableLabelFormat* format=nullptr)
+    static void loadLabel(QLabel* label, type* widget, EditableLabelFormatter* formatter=nullptr)
     {
-        std::ignore=format;
-        label->setText(QString("%1").arg(widget->value()));
+        using valueType=decltype(widget->value());
+
+        EditableLabelFormatter::loadLabel<EditableLabel::Type::Int,valueType>(label,formatter,[widget](){return widget->value();},[](const valueType& val){return QString::number(val);});
     }
 
     static auto value(const type* widget)
@@ -157,10 +209,11 @@ struct EditableLabelTraits<EditableLabel::Type::Float>
 {
     using type=QDoubleSpinBox;
 
-    static void loadLabel(QLabel* label, type* widget, EditableLabelFormat* format=nullptr)
+    static void loadLabel(QLabel* label, type* widget, EditableLabelFormatter* formatter=nullptr)
     {
-        std::ignore=format;
-        label->setText(QString("%1").arg(widget->value()));
+        using valueType=decltype(widget->value());
+
+        EditableLabelFormatter::loadLabel<EditableLabel::Type::Float,valueType>(label,formatter,[widget](){return widget->value();},[](const valueType& val){return QString::number(val);});
     }
 
     static auto value(const type* widget)
@@ -174,7 +227,7 @@ struct EditableLabelTraits<EditableLabel::Type::Float>
     }
 };
 
-struct EditableLabelListValue
+struct UISE_DESKTOP_EXPORT EditableLabelListValue
 {
     int index;
     QString text;
@@ -186,10 +239,9 @@ struct EditableLabelTraits<EditableLabel::Type::List>
 {
     using type=QComboBox;
 
-    static void loadLabel(QLabel* label, type* widget, EditableLabelFormat* format=nullptr)
+    static void loadLabel(QLabel* label, type* widget, EditableLabelFormatter* formatter=nullptr)
     {
-        std::ignore=format;
-        label->setText(widget->currentText());
+        EditableLabelFormatter::loadLabel<EditableLabel::Type::List,QString>(label,formatter,[widget](){return widget->currentText();},[](const QString& val){return val;});
     }
 
     static auto value(const type* widget)
@@ -206,6 +258,11 @@ struct EditableLabelTraits<EditableLabel::Type::List>
     {
         widget->setCurrentText(value);
     }
+
+    static void setValue(type* widget, const EditableLabelListValue& value)
+    {
+        widget->setCurrentIndex(value.index);
+    }
 };
 
 template <>
@@ -213,10 +270,11 @@ struct EditableLabelTraits<EditableLabel::Type::Date>
 {
     using type=QDateEdit;
 
-    static void loadLabel(QLabel* label, const type* widget, EditableLabelFormat* format=nullptr)
+    static void loadLabel(QLabel* label, const type* widget, EditableLabelFormatter* formatter=nullptr)
     {
-        std::ignore=format;
-        label->setText(widget->date().toString());
+        using valueType=decltype(widget->date());
+
+        EditableLabelFormatter::loadLabel<EditableLabel::Type::Date,valueType>(label,formatter,[widget](){return widget->date();},[](const valueType& val){return val.toString();});
     }
 
     static auto value(const type* widget)
@@ -235,9 +293,11 @@ struct EditableLabelTraits<EditableLabel::Type::Time>
 {
     using type=QTimeEdit;
 
-    static void loadLabel(QLabel* label, const type* widget, EditableLabelFormat* format=nullptr)
+    static void loadLabel(QLabel* label, const type* widget, EditableLabelFormatter* formatter=nullptr)
     {
-        label->setText(widget->time().toString());
+        using valueType=decltype(widget->time());
+
+        EditableLabelFormatter::loadLabel<EditableLabel::Type::Time,valueType>(label,formatter,[widget](){return widget->time();},[](const valueType& val){return val.toString();});
     }
 
     static auto value(const type* widget)
@@ -256,9 +316,11 @@ struct EditableLabelTraits<EditableLabel::Type::DateTime>
 {
     using type=QDateTimeEdit;
 
-    static void loadLabel(QLabel* label, const type* widget, EditableLabelFormat* format=nullptr)
+    static void loadLabel(QLabel* label, const type* widget, EditableLabelFormatter* formatter=nullptr)
     {
-        label->setText(widget->dateTime().toString());
+        using valueType=decltype(widget->dateTime());
+
+        EditableLabelFormatter::loadLabel<EditableLabel::Type::DateTime,valueType>(label,formatter,[widget](){return widget->dateTime();},[](const valueType& val){return val.toString();});
     }
 
     static auto value(const type* widget)
@@ -275,10 +337,10 @@ struct EditableLabelTraits<EditableLabel::Type::DateTime>
 namespace detail
 {
 
-template <EditableLabel::Type Type>
+template <EditableLabel::Type TypeId>
 struct EditableLabelHelper
 {
-    using traits = EditableLabelTraits<Type>;
+    using traits = EditableLabelTraits<TypeId>;
     using type = typename traits::type;
 
     static auto createWidget(QWidget* parent=nullptr)
@@ -286,9 +348,9 @@ struct EditableLabelHelper
         return new type(parent);
     }
 
-    static void loadLabel(QLabel* label, type* widget, EditableLabelFormat* format=nullptr)
+    static void loadLabel(QLabel* label, type* widget, EditableLabelFormatter* formatter=nullptr)
     {
-        traits::loadLabel(label, widget, format);
+        traits::loadLabel(label, widget, formatter);
     }
 
     static auto value(const type* widget)
@@ -297,13 +359,241 @@ struct EditableLabelHelper
     }
 
     template <typename ValueType>
-    void setValue(type* widget, const ValueType& value)
+    static void setValue(type* widget, const ValueType& value)
     {
         traits::setValue(widget,value);
     }
 };
 
+template <EditableLabel::Type TypeId>
+class EditableLabelTmpl : public EditableLabel
+{
+    public:
+
+        using helper = EditableLabelHelper<TypeId>;
+        using widgetType = typename helper::type;
+
+        EditableLabelTmpl(QWidget* parent=nullptr)
+            : EditableLabel(TypeId),
+              m_widget(helper::createWidget(parent))
+        {
+            boxLayout()->addWidget(m_widget);
+            m_widget->setVisible(false);
+
+            m_widget->setObjectName("widget");
+            m_widget->installEventFilter(this);
+        }
+
+        auto value() const
+        {
+           return helper::value(m_widget);
+        }
+
+        template <typename ValueType>
+        void setValue(const ValueType& value)
+        {
+            m_widget->blockSignals(true);
+            helper::setValue(m_widget,value);
+            helper::loadLabel(label(),m_widget,formatter());
+            m_backupValue=helper::value(m_widget);
+            m_widget->blockSignals(false);
+        }
+
+        QWidget* editableWidget() const noexcept override
+        {
+            return m_widget;
+        }
+
+        auto widget() const noexcept
+        {
+            return m_widget;
+        }
+
+        virtual void apply() override
+        {
+            m_backupValue=helper::value(m_widget);
+            helper::loadLabel(label(),m_widget,formatter());
+            emit valueSet();
+            notifyValueChanged();
+        }
+
+    protected:
+
+        virtual void notifyValueChanged()=0;
+
+        virtual void restoreWidgetValue() override
+        {
+            m_widget->blockSignals(true);
+            helper::setValue(m_widget,m_backupValue);
+            m_widget->blockSignals(false);
+        }
+
+    private:
+
+        typename helper::type *m_widget;
+
+        using keepValueType=decltype(helper::value(std::declval<typename helper::type*>()));
+
+        keepValueType m_backupValue;
+};
+
 }
+
+class UISE_DESKTOP_EXPORT EditableLabelText : public detail::EditableLabelTmpl<EditableLabel::Type::Text>
+{
+    Q_OBJECT
+
+    public:
+
+        using baseType = detail::EditableLabelTmpl<EditableLabel::Type::Text>;
+
+        using baseType::baseType;
+
+    signals:
+
+        void valueChanged(const QString& text);
+
+    protected:
+
+        virtual void notifyValueChanged() override
+        {
+            emit valueChanged(widget()->text());
+        }
+};
+
+class UISE_DESKTOP_EXPORT EditableLabelInt : public detail::EditableLabelTmpl<EditableLabel::Type::Int>
+{
+    Q_OBJECT
+
+    public:
+
+        using baseType = detail::EditableLabelTmpl<EditableLabel::Type::Int>;
+
+        using baseType::baseType;
+
+    signals:
+
+        void valueChanged(int value);
+
+    protected:
+
+        virtual void notifyValueChanged() override
+        {
+            emit valueChanged(widget()->value());
+        }
+};
+
+class UISE_DESKTOP_EXPORT EditableLabelFloat : public detail::EditableLabelTmpl<EditableLabel::Type::Float>
+{
+    Q_OBJECT
+
+    public:
+
+        using baseType = detail::EditableLabelTmpl<EditableLabel::Type::Float>;
+
+        using baseType::baseType;
+
+    signals:
+
+        void valueChanged(double value);
+
+    protected:
+
+        virtual void notifyValueChanged() override
+        {
+            emit valueChanged(widget()->value());
+        }
+};
+
+class UISE_DESKTOP_EXPORT EditableLabelDate : public detail::EditableLabelTmpl<EditableLabel::Type::Date>
+{
+    Q_OBJECT
+
+    public:
+
+        using baseType = detail::EditableLabelTmpl<EditableLabel::Type::Date>;
+
+        using baseType::baseType;
+
+    signals:
+
+        void valueChanged(const QDate& value);
+
+    protected:
+
+        virtual void notifyValueChanged() override
+        {
+            emit valueChanged(widget()->date());
+        }
+};
+
+class UISE_DESKTOP_EXPORT EditableLabelTime: public detail::EditableLabelTmpl<EditableLabel::Type::Time>
+{
+    Q_OBJECT
+
+    public:
+
+        using baseType = detail::EditableLabelTmpl<EditableLabel::Type::Time>;
+
+        using baseType::baseType;
+
+    signals:
+
+        void valueChanged(const QTime& value);
+
+    protected:
+
+        virtual void notifyValueChanged() override
+        {
+            emit valueChanged(widget()->time());
+        }
+};
+
+class UISE_DESKTOP_EXPORT EditableLabelDateTime: public detail::EditableLabelTmpl<EditableLabel::Type::DateTime>
+{
+    Q_OBJECT
+
+    public:
+
+        using baseType = detail::EditableLabelTmpl<EditableLabel::Type::DateTime>;
+
+        using baseType::baseType;
+
+    signals:
+
+        void valueChanged(const QDateTime& value);
+
+    protected:
+
+        virtual void notifyValueChanged() override
+        {
+            emit valueChanged(widget()->dateTime());
+        }
+};
+
+class UISE_DESKTOP_EXPORT EditableLabelList: public detail::EditableLabelTmpl<EditableLabel::Type::List>
+{
+    Q_OBJECT
+
+    public:
+
+        using baseType = detail::EditableLabelTmpl<EditableLabel::Type::List>;
+
+        using baseType::baseType;
+
+    signals:
+
+        void indexChanged(int index);
+        void textChanged(const QString& text);
+
+    protected:
+
+        virtual void notifyValueChanged() override
+        {
+            emit indexChanged(widget()->currentIndex());
+            emit textChanged(widget()->currentText());
+        }
+};
 
 UISE_DESKTOP_NAMESPACE_END
 
---
+++ src/editablelabel.cpp
@@ -0,0 +1,77 @@
+/**
+@copyright Evgeny Sidorov 2021
+
+This software is dual-licensed. Choose the appropriate license for your project.
+
+1. For open source projects this software is distrubuted under the GNU GENERAL PUBLIC LICENSE, Version 3.0.
+(See accompanying file LICENSE-GPLv3.md or copy at https://www.gnu.org/licenses/gpl-3.0.txt)
+
+2. For proprietary license contact evgeniums@dracosha.com.
+
+*/
+
+/****************************************************************************/
+
+/** @file uise/desktop/src/editablelabel.cpp
+*
+*  Defines AlignedEditableLabel.
+*
+*/
+
+/****************************************************************************/
+
+#include <QMainWindow>
+#include <QKeyEvent>
+
+#include <uise/desktop/utils/layout.hpp>
+#include <uise/desktop/editablelabel.hpp>
+
+UISE_DESKTOP_NAMESPACE_BEGIN
+
+//--------------------------------------------------------------------------
+EditableLabel::EditableLabel(
+        Type type,
+        QWidget* parent
+    ) : QFrame(parent),
+        m_type(type),
+        m_label(new QLabel(this)),
+        m_formatter(nullptr)
+{
+    m_layout=Layout::create<QVBoxLayout>(this);
+    m_layout->addWidget(m_label);
+
+    m_label->setObjectName("label");
+    m_label->setTextInteractionFlags(Qt::TextSelectableByMouse | Qt::TextSelectableByKeyboard);
+
+    m_label->installEventFilter(this);
+}
+
+//--------------------------------------------------------------------------
+bool EditableLabel::eventFilter(QObject *watched, QEvent *event)
+{
+    if (watched == editableWidget() && event->type() == QEvent::KeyPress)
+    {
+        QKeyEvent *keyEvent = static_cast<QKeyEvent*>(event);
+        if (keyEvent->key()==Qt::Key_Escape)
+        {
+            cancel();
+            return true;
+        }
+        if (keyEvent->key()==Qt::Key_Return)
+        {
+            apply();
+            return true;
+        }
+    }
+    else if (watched == m_label && event->type() == QEvent::MouseButtonDblClick)
+    {
+        setEditable(true);
+        return true;
+    }
+
+    return false;
+}
+
+//--------------------------------------------------------------------------
+
+UISE_DESKTOP_NAMESPACE_END
