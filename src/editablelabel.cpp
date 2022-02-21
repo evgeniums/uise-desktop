b1ff74b39c82bd0df68214b06a98b7560343ab5d
Evgeny Sidorov <evgeniums@dracosha.com>
2022-02-21 23:12:30 +0300

    Set object names for buttons

--- src/editablelabel.cpp
+++ src/editablelabel.cpp
@@ -52,8 +52,9 @@ EditableLabel::EditableLabel(
     m_editButton->setIcon(editIcon);
     m_editButton->setSizePolicy(QSizePolicy::Fixed,QSizePolicy::Fixed);
     m_editButton->setFlat(true);
-    m_editButton->setProperty("label-button",true);
+    m_editButton->setProperty("labelButton",true);
     m_editButton->setToolTip(tr("Edit"));
+    m_editButton->setObjectName("editButton");
     connect(m_editButton,&QPushButton::clicked,this,&EditableLabel::edit);
     m_layout->addWidget(m_editButton);
     m_editButton->setVisible(!m_inGroup);
@@ -63,8 +64,9 @@ EditableLabel::EditableLabel(
     m_applyButton->setIcon(applyIcon);
     m_applyButton->setSizePolicy(QSizePolicy::Fixed,QSizePolicy::Fixed);
     m_applyButton->setFlat(true);
-    m_applyButton->setProperty("label-button",true);
+    m_applyButton->setProperty("labelButton",true);
     m_applyButton->setToolTip(tr("Apply"));
+    m_applyButton->setObjectName("applyButton");
     connect(m_applyButton,&QPushButton::clicked,this,&EditableLabel::apply);
     m_layout->addWidget(m_applyButton);
     m_applyButton->setVisible(false);
@@ -74,8 +76,9 @@ EditableLabel::EditableLabel(
     m_cancelButton->setIcon(cancelIcon);
     m_cancelButton->setSizePolicy(QSizePolicy::Fixed,QSizePolicy::Fixed);
     m_cancelButton->setFlat(true);
-    m_cancelButton->setProperty("label-button",true);
+    m_cancelButton->setProperty("labelButton",true);
     m_cancelButton->setToolTip(tr("Cancel"));
+    m_cancelButton->setObjectName("cancelButton");
     m_cancelButton->setVisible(false);
     connect(m_cancelButton,&QPushButton::clicked,this,&EditableLabel::cancel);
     m_layout->addWidget(m_cancelButton);
