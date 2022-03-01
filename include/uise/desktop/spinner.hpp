c84deb9543cae5e9e086a3bc26a23a2b92c3c179
Evgeny Sidorov <evgeniums@dracosha.com>
2022-03-01 19:00:19 +0300

    Add doxygen comments for Spinner ans SpinnerSection

--- include/uise/desktop/spinner.hpp
+++ include/uise/desktop/spinner.hpp
@@ -35,6 +35,19 @@ UISE_DESKTOP_NAMESPACE_BEGIN
 class SpinnerSection;
 class Spinner_p;
 
+/**
+ * @brief Spinner is a view for representing lists of items that look like combo boxes (spinners) on smartphones.
+ *
+ * Spinner can represent a few item lists at once.
+ * Each list is processed by a spinner section representing a scrollable list of items.
+ * In addition, a section can be expanded with left and/or right bar containing static label in the center of a section.
+ *
+ * A spinner section can be either linear or circular which is enabled by SpinnerSection::setCircular().
+ * Different section of the same spinner can be of different modes.
+ *
+ * All items in all spinner sections have the same height that must be set with setItemHeight().
+ *
+ */
 class UISE_DESKTOP_EXPORT Spinner : public QFrame
 {
     Q_OBJECT
@@ -45,6 +58,10 @@ class UISE_DESKTOP_EXPORT Spinner : public QFrame
         inline static int DefaultSingleScrollStep=4;
         inline static int WheelScrollStep=4;
 
+        /**
+         * @brief Constructor.
+         * @param parent Parent widget.
+         */
         Spinner(QWidget* parent=nullptr);
         ~Spinner();
 
@@ -53,41 +70,121 @@ class UISE_DESKTOP_EXPORT Spinner : public QFrame
         Spinner& operator=(const Spinner&) = delete;
         Spinner& operator=(Spinner&&) = delete;
 
+        /**
+         * @brief Set sample widget to be used as style reference.
+         * @param widget Reference widget.
+         *
+         * The spinner will own sample widget.
+         */
         void setStyleSample(QWidget* widget);
 
+        /**
+         * @brief Load sections to spinner.
+         * @param sections Spinner sections.
+         *
+         * Note that the sections must be populated before loading to spinner.
+         */
         void setSections(std::vector<std::shared_ptr<SpinnerSection>> sections);
+
+        /**
+         * @brief Get spinner section by index.
+         * @param index Section index.
+         * @return Query result.
+         */
         std::shared_ptr<SpinnerSection> section(int index) const;
+
+        /**
+         * @brief Get number of sections in spinner.
+         * @return Query result.
+         */
         size_t sectionCount() const noexcept;
 
+        /**
+         * @brief scroll Scroll spinner section by delta offset.
+         * @param sectionIndex Section index.
+         * @param delta Offset.
+         */
         void scroll(int sectionIndex, int delta)
         {
             scroll(section(sectionIndex).get(),delta);
         }
+
+        /**
+         * @brief Scroll spinner section to certain position.
+         * @param sectionIndex Section index.
+         * @param pos Target position.
+         */
         void scrollTo(int sectionIndex, int pos)
         {
             scrollTo(section(sectionIndex).get(),pos);
         }
 
+        /**
+         * @brief Get index of currently selected item in spinner section.
+         * @param sectionIndex Section index.
+         * @return Query result.
+         */
         int selectedItemIndex(int sectionIndex) const
         {
             return selectedItemIndex(section(sectionIndex).get());
         }
+
+        /**
+         * @brief Select item in a spinner section.
+         * @param sectionIndex Section index.
+         * @param index Item index.
+         */
         void selectItem(int sectionIndex, int index)
         {
             selectItem(section(sectionIndex).get(),index);
         }
 
+        /**
+         * @brief Set fixed height of spinner item.
+         * @param val Height of spinner item in pixels.
+         *
+         */
         void setItemHeight(int val) noexcept;
+
+        /**
+         * @brief Get height of a spinner item.
+         * @return Query result.
+         */
         int itemHeight() const noexcept;
 
+        /**
+         * @brief Append items to spinner section.
+         * @param section Section index.
+         * @param items List of widgets to append.
+         *
+         * The spinner will own appended items.
+         */
         void appendItems(int section, const QList<QWidget*>& items);
+
+        /**
+         * @brief Remove last items from spinner section.
+         * @param section Section index.
+         * @param count Number of items to remove.
+         */
         void removeLastItems(int section, int count);
 
+        /**
+         * @brief Append item to spinner section.
+         * @param section Section index.
+         * @param item Widget to append.
+         *
+         * The spinner will own appended item.
+         */
         void appendItem(int section, QWidget* item)
         {
             QList<QWidget*> items{item};
             appendItems(section,items);
         }
+
+        /**
+         * @brief Remove last item from spinner section.
+         * @param section Section index.
+         */
         void removeLastItem(int section)
         {
             removeLastItems(section,1);
@@ -97,6 +194,11 @@ class UISE_DESKTOP_EXPORT Spinner : public QFrame
 
     signals:
 
+        /**
+         * @brief Notify that item selection was changed in a section.
+         * @param sectionIndex Section index.
+         * @param itemIndex New item index.
+         */
         void itemChanged(int sectionIndex, int itemIndex);
 
     protected:
--- include/uise/desktop/spinnersection.hpp
+++ include/uise/desktop/spinnersection.hpp
@@ -39,10 +39,16 @@ namespace detail
 class SpinnerSection_p;
 };
 
+/**
+ * @brief Section of a Spinner.
+ */
 class UISE_DESKTOP_EXPORT SpinnerSection
 {
     public:
 
+        /**
+         * @brief Constructor.
+         */
         SpinnerSection();
 
         SpinnerSection(const SpinnerSection&) = delete;
@@ -50,25 +56,87 @@ class UISE_DESKTOP_EXPORT SpinnerSection
         SpinnerSection& operator=(const SpinnerSection&) = delete;
         SpinnerSection& operator=(SpinnerSection&&) = delete;
 
+        /**
+         * @brief Destructor.
+         */
         ~SpinnerSection();
 
+        /**
+         * @brief Full width of the section.
+         * @return
+         */
         int width() const noexcept;
 
+        /**
+         * @brief Load list of items to section.
+         * @param items List of widgets.
+         */
         void setItems(QList<QWidget*> items);
+
+        /**
+         * @brief Set widget to be used as a selection label in the left bar.
+         * @param widget Bar label
+         */
         void setLeftBarLabel(QWidget* widget=nullptr) noexcept;
+
+        /**
+         * @brief Set widget to be used as a selection label in the right bar.
+         * @param widget Bar label
+         */
         void setRightBarLabel(QWidget* widget=nullptr) noexcept;
 
+        /**
+         * @brief Set width of items list.
+         * @param val Width in pixels.
+         */
         void setItemsWidth(int val) noexcept;
+
+        /**
+         * @brief Get width of items list.
+         * @return Width in pixels.
+         */
         int itemsWidth() const noexcept;
+
+        /**
+         * @brief Get number of items in the section.
+         * @return Query result.
+         */
         int itemsCount() const noexcept;
 
+        /**
+         * @brief Set width of left bar.
+         * @param val Width in pixels.
+         */
         void setLeftBarWidth(int val) noexcept;
+
+        /**
+         * @brief Get width of left bar.
+         * @return Width in pixels.
+         */
         int leftBarWidth() const noexcept;
 
+        /**
+         * @brief Set width of right bar.
+         * @param val Width in pixels.
+         */
         void setRightBarWidth(int val) noexcept;
+
+        /**
+         * @brief Get width of right bar.
+         * @return Width in pixels.
+         */
         int rightBarWidth() const noexcept;
 
+        /**
+         * @brief Set circular mode of the section.
+         * @param enable Mode.
+         */
         void setCircular(bool enable) noexcept;
+
+        /**
+         * @brief Check if this section is in circular mode.
+         * @return True if section is in circular mode.
+         */
         bool circular() const noexcept;
 
     private:
