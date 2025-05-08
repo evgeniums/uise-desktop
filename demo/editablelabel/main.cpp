87e1be97bf82582dd4f333dedb70faaac454795a
evgeniums <evgeniums@dracosha.com>
2025-05-08 18:02:20 +0300

    Update license

--- demo/editablelabel/main.cpp
+++ demo/editablelabel/main.cpp
@@ -3,10 +3,13 @@
 
 This software is dual-licensed. Choose the appropriate license for your project.
 
-1. For open source projects this software is distrubuted under the GNU GENERAL PUBLIC LICENSE, Version 3.0.
-(See accompanying file LICENSE-GPLv3.md or copy at https://www.gnu.org/licenses/gpl-3.0.txt)
+1. The GNU GENERAL PUBLIC LICENSE, Version 3.0
+     (see accompanying file [LICENSE-GPLv3.md](LICENSE-GPLv3.md) or copy at https://www.gnu.org/licenses/gpl-3.0.txt)
+    
+2. The GNU LESSER GENERAL PUBLIC LICENSE, Version 3.0
+     (see accompanying file [LICENSE-LGPLv3.md](LICENSE-LGPLv3.md) or copy at https://www.gnu.org/licenses/lgpl-3.0.txt).
 
-2. For proprietary license contact evgeniums@dracosha.com.
+You may select, at your option, one of the above-listed licenses.
 
 */
 
