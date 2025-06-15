5beaebeb0d1b143ff304bc2f9ffc62287edb3c28
evgeniums <evgeniums@dracosha.com>
2025-06-15 20:22:10 +0300

    Add valuewidget.hpp

---
+++ include/uise/desktop/valuewidget.hpp
@@ -0,0 +1,126 @@
+/**
+@copyright Evgeny Sidorov 2021
+
+This software is dual-licensed. Choose the appropriate license for your project.
+
+1. The GNU GENERAL PUBLIC LICENSE, Version 3.0
+     (see accompanying file [LICENSE-GPLv3.md](LICENSE-GPLv3.md) or copy at https://www.gnu.org/licenses/gpl-3.0.txt)
+    
+2. The GNU LESSER GENERAL PUBLIC LICENSE, Version 3.0
+     (see accompanying file [LICENSE-LGPLv3.md](LICENSE-LGPLv3.md) or copy at https://www.gnu.org/licenses/lgpl-3.0.txt).
+
+You may select, at your option, one of the above-comboed licenses.
+
+*/
+
+/****************************************************************************/
+
+/** @file uise/desktop/valuewidget.hpp
+*
+*  Declares value widget.
+*
+*/
+
+/****************************************************************************/
+
+#ifndef UISE_DESKTOP_VALUEWIDGET_HPP
+#define UISE_DESKTOP_VALUEWIDGET_HPP
+
+#include <map>
+
+#include <QVariant>
+#include <QWidget>
+
+#include <uise/desktop/uisedesktop.hpp>
+
+UISE_DESKTOP_NAMESPACE_BEGIN
+
+class ValueWidgetConfig
+{
+    public:
+
+        ValueWidgetConfig()
+        {}
+
+        ValueWidgetConfig(std::map<int,QVariant> properties)
+            : m_properties(std::move(properties))
+        {}
+
+        QVariant property(int propertyId, QVariant defaultValue={}) const
+        {
+            auto it=m_properties.find(propertyId);
+            if (it!=m_properties.end())
+            {
+                return it->second;
+            }
+            return defaultValue;
+        }
+
+        template <typename T>
+        QVariant property(T propertyId, QVariant defaultValue={}) const
+        {
+            return property(static_cast<int>(propertyId),std::move(defaultValue));
+        }
+
+        void setProperty(int id, QVariant& value)
+        {
+            m_properties.emplace(id,std::move(value));
+        }
+
+    private:
+
+        std::map<int,QVariant> m_properties;
+};
+
+enum class ValueWidgetProperty : int
+{
+    Label,
+    Comment,
+    Alignment,
+    ColumnSpan,
+    RowSpan,
+    Min,
+    Max,
+    ListItems,
+
+    User=0x1000
+};
+
+class AbstractValueWidget : public QWidget
+{
+    public:
+
+        using QWidget::QWidget;
+
+        virtual void setVariantValue(const QVariant& val)=0;
+        virtual QVariant variantValue() const=0;
+
+        void setConfig(ValueWidgetConfig config)
+        {
+            m_config=std::move(config);
+            updateConfig();
+        }
+
+        const ValueWidgetConfig& config() const
+        {
+            return m_config;
+        }
+
+        ValueWidgetConfig& config()
+        {
+            return m_config;
+        }
+
+    protected:
+
+        virtual void updateConfig()
+        {}
+
+    private:
+
+        ValueWidgetConfig m_config;
+};
+
+UISE_DESKTOP_NAMESPACE_END
+
+#endif // UISE_DESKTOP_VALUEWIDGET_HPP
