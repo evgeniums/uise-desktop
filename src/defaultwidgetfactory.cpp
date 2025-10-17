56f0325a49fe53b05bf4da0b92b28eeb67a1cae1
evgeniums <evgeniums@dracosha.com>
2025-10-17 18:54:02 +0300

    Add loading frame to default widget factory

--- src/defaultwidgetfactory.cpp
+++ src/defaultwidgetfactory.cpp
@@ -26,6 +26,7 @@ You may select, at your option, one of the above-listed licenses.
 #include <uise/desktop/statusdialog.hpp>
 #include <uise/desktop/editablepanelgrid.hpp>
 #include <uise/desktop/editablepanels.hpp>
+#include <uise/desktop/loadingframe.hpp>
 
 #include <uise/desktop/defaultwidgetfactory.hpp>
 
@@ -40,6 +41,8 @@ std::shared_ptr<WidgetFactory> defaultWidgetFactory()
     factory->registerBuilder<AbstractStatusDialog>([](QWidget* parent){return new StatusDialog(parent);});
     factory->registerBuilder<AbstractEditablePanel>([](QWidget* parent){return new EditablePanelGrid(parent);});
     factory->registerBuilder<AbstractEditablePanels>([](QWidget* parent){return new EditablePanels(parent);});
+
+    factory->registerBuilder<LoadingFrame>([](QWidget* parent){return new LoadingFrame(parent);});
 
     return factory;
 }
